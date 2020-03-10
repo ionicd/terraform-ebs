edefdcf86be5238ac35f061276e7300f8ddffa26
Saurav KC <Saurav.KC14@myhunter.cuny.edu>
2020-03-10 19:50:25 -0400

    Update user-data.sh

--- script/user-data.sh
+++ script/user-data.sh
@@ -1,1 +1,19 @@
+vol=vol-0e2e52c0261bc58fa
+mount_dir=/saurav
+device=/dev/xvdf/
 
+if [ ! -d $mount_dir ]
+then
+mkdir $mount_dir
+fi
+
+echo "this command is to fetch instance id"
+instance_id=`curl http://169.254.169.254/latest/meta-data/instance-id`
+
+echo "this command is to attach EBS to EC2"
+aws ec2 attach-volume --volume-id $vol --instance-id $instance_id --device /dev/sdf
+
+sleep 60 
+
+echo "mount filesystem to folder" 
+mount $device $mount_dir
